7225eb2c91e38997baa76188ae6053892c4af8ff
Sebastian Bindgen <info@bindgen.net>
2018-08-07 18:36:03 +0200

    Added function in lees_edwards.cpp to update the protocol function based on the chosen protocol, time and box_l

--- src/core/lees_edwards.cpp
+++ src/core/lees_edwards.cpp
@@ -20,6 +20,37 @@
  *
  */
 
+#include <cmath>
 #include "lees_edwards.hpp"
+#include "integrate.hpp"
 
 lees_edwards_protocol_struct lees_edwards_protocol;
+
+/* Functions to determine the current offset and shear rate with respect to the chosen protocol */
+
+void setup_lees_edwards_protocol(lees_edwards_protocol_struct lees_edwards_protocol, double sim_time, double box_l_y) {
+  if (lees_edwards_protocol.type == 0 ) {
+    //lees_edwards_protocol.offset = Current value;
+    lees_edwards_protocol.velocity = 0.0;
+    lees_edwards_protocol.amplitude = 0.0;
+    lees_edwards_protocol.frequency = 0.0;
+  } 
+
+
+  if (lees_edwards_protocol.type == 1 ) {
+    lees_edwards_protocol.velocity = 0.0;
+    lees_edwards_protocol.amplitude = 0.0;
+    lees_edwards_protocol.frequency = 0.0;
+  } 
+
+  if (lees_edwards_protocol.type == 2 ) {
+    lees_edwards_protocol.offset = (lees_edwards_protocol.velocity / box_l_y) * sim_time;
+    lees_edwards_protocol.amplitude = 0.0;
+    lees_edwards_protocol.frequency = 0.0;
+  } 
+  
+  if (lees_edwards_protocol.type == 3 ) {
+    lees_edwards_protocol.offset = (lees_edwards_protocol.amplitude / box_l_y) * std::sin(lees_edwards_protocol.frequency*sim_time);
+    lees_edwards_protocol.velocity = (lees_edwards_protocol.amplitude / box_l_y) * std::cos(lees_edwards_protocol.frequency*sim_time); 
+  } 
+}
